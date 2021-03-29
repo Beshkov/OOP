cb63a40d14af324480d4defc06a93c5ce5ca209b
Beshkov <alexander.beshkov@gmail.com>
2021-03-30 00:27:37 +0300

    .

---
+++ 02-Classes-and-Instances/exer-04-employee.py
@@ -0,0 +1,24 @@
+class Employee:
+    def __init__(self, id, first_name, last_name, salary):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.salary = salary
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_annual_salary(self):
+        return self.salary * 12
+
+    def raise_salary(self, amount):
+        # self note: the amount should be checked you can't raise salary with negative number, so my refactoring:
+        if amount <= 0:
+            return f"You cant raise salary by negative amount: {amount}"
+        self.salary += amount
+        return self.salary
+
+employee = Employee(744423129, "John", "Smith", 1000)
+print(employee.get_full_name())
+print(employee.raise_salary(500))
+print(employee.get_annual_salary())
